a11f01282164434f6cc38f6b85356e663dd92510
xeeraki <shafai86@gmail.com>
2021-10-13 14:04:44 +0200

    .net 5 (previuose name asp.net core)

---
+++ DatingApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Models
+{
+    public class AppUser
+    {
+
+        public int Id { get; set; }
+    }
+}
